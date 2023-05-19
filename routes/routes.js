4c0aadb248f3b8724fe2aabab9503052f578cdec
Lin <47834369+lin-hikari@users.noreply.github.com>
2023-05-19 09:42:13 -0300

    fixed router name reference
    because im a silly dumdum who doesnt test things before committing :))

--- routes/routes.js
+++ routes/routes.js
@@ -4,11 +4,11 @@ const controller = require("../controllers/controller");
 
 const router = express.Router();
 
-router.post("/create-folder", testController.createFolder);
+router.post("/create-folder", controller.createFolder);
 
-router.post("/add-chip-to-folder", testController.addChipToFolder);
+router.post("/add-chip-to-folder", controller.addChipToFolder);
 
-router.post("/remove-chip-from-folder", testController.removeChipFromFolder);
+router.post("/remove-chip-from-folder", controller.removeChipFromFolder);
 
 //router.get('/chips', testController.getChips);
 
