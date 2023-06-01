a36f816ba9d8b61d3e690d772ab7253fdaccf7bb
Lin <47834369+lin-hikari@users.noreply.github.com>
2023-06-01 17:40:55 -0300

    Commented out tests which are not implemented yet
    So it doesn't fill the tests with useless info

--- test/folder-controller-test.js
+++ test/folder-controller-test.js
@@ -110,53 +110,53 @@ describe("Folder Controller", function () {
     it("should throw an error (500) if no folder description is provided", function () {});
   });
 
-  describe("Delete Folder", function () {
-    it("should throw an error (400) if user is not found", function () {});
+  // describe("Delete Folder", function () {
+  //   it("should throw an error (400) if user is not found", function () {});
 
-    it("should throw an error (404) if folder is not found", function () {});
+  //   it("should throw an error (404) if folder is not found", function () {});
 
-    it("should throw an error (401) if folder does not belong to user", function () {});
+  //   it("should throw an error (401) if folder does not belong to user", function () {});
 
-    it("should remove the folder reference to the user who created it", function () {});
+  //   it("should remove the folder reference to the user who created it", function () {});
 
-    it("should send a success message (200) if folder is deleted successfully", function () {});
-  });
+  //   it("should send a success message (200) if folder is deleted successfully", function () {});
+  // });
 
-  describe("View Folder", function () {
-    it("should throw an error (422) if folder id format is invalid", function () {});
+  // describe("View Folder", function () {
+  //   it("should throw an error (422) if folder id format is invalid", function () {});
 
-    it("should throw an error (404) if folder is not found", function () {});
+  //   it("should throw an error (404) if folder is not found", function () {});
 
-    it("should return the folder with populated info of chips", function () {});
+  //   it("should return the folder with populated info of chips", function () {});
 
-    it("should send a success message (200) if folder data is returned successfully", function () {});
-  });
+  //   it("should send a success message (200) if folder data is returned successfully", function () {});
+  // });
 
-  describe("Add Chip to Folder", function () {
-    it("should throw an error (404) if folder is not found", function () {});
+  // describe("Add Chip to Folder", function () {
+  //   it("should throw an error (404) if folder is not found", function () {});
 
-    it("should throw an error (401) if folder does not belong to user", function () {});
+  //   it("should throw an error (401) if folder does not belong to user", function () {});
 
-    it("should throw an error (404) if chip is not found", function () {});
+  //   it("should throw an error (404) if chip is not found", function () {});
 
-    it("should throw an error (400) if folder is full", function () {});
+  //   it("should throw an error (400) if folder is full", function () {});
 
-    it("should add to the folder a reference of the chip added", function () {});
+  //   it("should add to the folder a reference of the chip added", function () {});
 
-    it("should send a success message (200) if chip is added to folder successfully", function () {});
-  });
+  //   it("should send a success message (200) if chip is added to folder successfully", function () {});
+  // });
 
-  describe("Remove Chip from Folder", function () {
-    it("should throw an error (404) if folder is not found", function () {});
+  // describe("Remove Chip from Folder", function () {
+  //   it("should throw an error (404) if folder is not found", function () {});
 
-    it("should throw an error (401) if folder does not belong to user", function () {});
+  //   it("should throw an error (401) if folder does not belong to user", function () {});
 
-    it("should throw an error (404) if chip is not found", function () {});
+  //   it("should throw an error (404) if chip is not found", function () {});
 
-    it("should throw an error (404) if chip is not found in folder", function () {});
+  //   it("should throw an error (404) if chip is not found in folder", function () {});
 
-    it("should remove from the folder the reference of the chip removed", function () {});
+  //   it("should remove from the folder the reference of the chip removed", function () {});
 
-    it("should send a success message (200) if chip is removed from folder successfully", function () {});
-  });
+  //   it("should send a success message (200) if chip is removed from folder successfully", function () {});
+  // });
 });
